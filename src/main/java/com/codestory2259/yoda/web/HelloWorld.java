a2b4a52c14aa46d22965653a2ac54f816b6c2df3
Thierry Nowak <thinow@gmail.com>
2015-06-28 22:19:58 +0200

    use a property

--- src/main/java/com/codestory2259/yoda/web/HelloWorld.java
+++ src/main/java/com/codestory2259/yoda/web/HelloWorld.java
@@ -1,19 +1,25 @@
 package com.codestory2259.yoda.web;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import static java.lang.String.format;
+
 @Controller
 @EnableAutoConfiguration
 public class HelloWorld {
 
+    @Value("${helloworld.name}")
+    private String name;
+
     @RequestMapping("/")
     @ResponseBody
     String sayHello() {
-        return "Hey guys!";
+        return format("Hey %s!", name);
     }
 
     public static void main(String... args) {
