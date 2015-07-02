95fed16c4dd9168e25d11f11c38bd1043c0f257f
Code Story Team <codestory2259@mail.com>
2015-07-02 13:30:31 +0200

    Create main class

--- src/main/java/com/codestory2259/yoda/web/Rest.java
+++ src/main/java/com/codestory2259/yoda/web/Rest.java
@@ -1,4 +1,14 @@
 package com.codestory2259.yoda.web;
 
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+
+@EnableAutoConfiguration
 public class Rest {
+
+
+    public static void main(String... args) {
+        SpringApplication.run(Rest.class);
+    }
 }
