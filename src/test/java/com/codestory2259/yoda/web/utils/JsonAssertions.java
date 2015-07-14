7e70b35781ab2ca5bcfb00710f2578c32c015924
Thierry Nowak <thinow@gmail.com>
2015-07-14 12:52:56 +0200

    refactor JsonAssertions

--- src/test/java/com/codestory2259/yoda/web/utils/JsonAssertions.java
+++ src/test/java/com/codestory2259/yoda/web/utils/JsonAssertions.java
@@ -1,15 +1,25 @@
 package com.codestory2259.yoda.web.utils;
 
+import com.jayway.jsonpath.JsonPath;
 import org.assertj.core.api.AbstractObjectAssert;
-import org.assertj.core.api.Assertions;
 
-import static com.jayway.jsonpath.JsonPath.read;
+import static java.lang.String.format;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class JsonAssertions {
 
-    public static AbstractObjectAssert<?, Object> assertThatJson(String response, String query) {
-        Object read = read(response, query);
-        return Assertions.assertThat(read);
+    public static AbstractObjectAssert<?, Object> assertThatJson(String json, String path) {
+        String description = format("JSON = %s", json);
+        return assertThat(parse(json, path)).as(description);
+    }
+
+    private static Object parse(String json, String path) {
+        try {
+            return JsonPath.read(json, path);
+        } catch (Exception cause) {
+            String message = format("Unexpected JSON format for `%s`", json);
+            throw new IllegalStateException(message, cause);
+        }
     }
 
 }
