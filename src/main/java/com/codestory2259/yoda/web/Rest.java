ef07144c030d914409c8e72ed2ca84c886e75da7
Thierry Nowak <thinow@gmail.com>
2015-07-14 11:32:31 +0200

    check rest annotations

--- src/main/java/com/codestory2259/yoda/web/Rest.java
+++ src/main/java/com/codestory2259/yoda/web/Rest.java
@@ -3,8 +3,11 @@ package com.codestory2259.yoda.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @EnableAutoConfiguration
+@RestController
 public class Rest {
 
     public static final String STATUS_OK = "{\"status\":\"OK\"}";
@@ -13,6 +16,7 @@ public class Rest {
         SpringApplication.run(Rest.class);
     }
 
+    @RequestMapping(produces = "application/json")
     public String status() {
         return STATUS_OK;
     }
--- src/test/java/com/codestory2259/yoda/web/RestTest.java
+++ src/test/java/com/codestory2259/yoda/web/RestTest.java
@@ -2,12 +2,15 @@ package com.codestory2259.yoda.web;
 
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.assertj.core.api.Assertions;
 import org.junit.Test;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class RestTest {
 
@@ -22,6 +25,12 @@ public class RestTest {
         verifyJsonFormat(status);
     }
 
+    private void verifyJsonFormat(String status) throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        JsonParser parser = mapper.getFactory().createParser(status);
+        while (parser.nextToken() != null) ;
+    }
+
     @Test
     public void statusSendAlwaysOK() throws Exception {
         // when
@@ -31,9 +40,27 @@ public class RestTest {
         assertThat(status).contains("status").contains("OK");
     }
 
-    private void verifyJsonFormat(String status) throws IOException {
-        ObjectMapper mapper = new ObjectMapper();
-        JsonParser parser = mapper.getFactory().createParser(status);
-        while (parser.nextToken() != null) ;
+    @Test
+    public void restMapping() throws Exception {
+
+        Class<? extends Rest> type = rest.getClass();
+        String expectedName = "status";
+
+
+
+
+        RestController annotation = type.getAnnotation(RestController.class);
+        assertThat(annotation).as("The class must be annotated @RestController").isNotNull();
+
+        Method method = Arrays.stream(type.getMethods())
+                .filter(m -> m.getName().equals(expectedName))
+                .findFirst()
+                .get();
+
+        assertThat(method).as("Method must exist").isNotNull();
+
+        RequestMapping annotation2 = method.getAnnotation(RequestMapping.class);
+        assertThat(annotation2).as("The method must be annotated @RequestMapping").isNotNull();
+        assertThat(annotation2.produces()).contains("application/json");
     }
 }
